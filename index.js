72f6dbe0044236292534d88e01ccab5238da76b8
Greg Connell <greg.connell@ff0000.com>
2018-01-31 15:50:00 -0800

    adds StaticGenerator, which got lost in migration

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-export { default as Device } from './lib/Device.js'
-export { default as Velvet } from './lib/Velvet.js'
+export {default as Device} from './lib/Device.js';
+export {default as Velvet} from './lib/Velvet.js';
+export {default as StaticGenerator} from './lib/StaticGenerator.js';
