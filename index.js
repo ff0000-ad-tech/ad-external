f6c5918627658a84da1f1ebbfe8fa1e4e504ed2d
Kenny Arehart <kenny.arehart@ff0000.com>
2018-05-11 17:40:29 -0700

    removed index exports for Velvet and StaticGenerator

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 export { default as Device } from './lib/Device.js'
-
-import * as Velvet from './lib/Velvet.js'
-import * as StaticGenerator from './lib/StaticGenerator.js'
-export { StaticGenerator, Velvet }
